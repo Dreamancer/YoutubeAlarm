63ce037d64234ca9c1bf7faea38deaeeb86fff87
Jakub Horniak <horniakj@gmail.com>
2016-07-22 13:16:23 +0200

    init
    asdf

---
+++ YoutubeAlarm/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace YoutubeAlarm
+{
+    [Activity(Label = "YoutubeAlarm", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+            EditText edText = FindViewById<EditText>(Resource.Id.queryText);
+
+            button.Click += async delegate {
+                var searchListResult = await YTHelper.Search(edText.Text, 50);
+                Wait(100);
+
+            };
+        }
+    }
+}
+
